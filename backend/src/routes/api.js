8f514f75680886f36ea9f8ca4784494e5a347c41
Philipp Sum <psum@Mac.fritz.box>
2025-03-23 21:48:09 +0100

    update
    API working as expected.
    Only issue: The fist letter, of sentences that are not the first one,
    are not capitalized

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -2,9 +2,41 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/', async (req, res) => {
+router.post('/translate_chicken', async (req, res) => {
     try {
-        res.send("Hello")
+        let { text } = req.body;
+        text = text.toLowerCase()
+        var chicken_text = "";
+        for (var i = 0; i < text.length; i++){
+            switch (text[i]) {
+                case "a":
+                    chicken_text += "ahadefa";
+                    break;
+                case "e":
+                    chicken_text += "ehedefe";
+                    break;
+                case "i":
+                    chicken_text += "ihidefi";
+                    break;
+                case "o":
+                    chicken_text += "ohodefo";
+                    break;
+                case "u":
+                    chicken_text += "uhudefu";
+                    break;
+                case "ä":
+                    chicken_text += "ähädefä";
+                    break;
+                default:
+                    chicken_text += text[i];
+            }
+    }
+        chicken_text = chicken_text.replace("ahadefauhudefu", "auhaudefau")
+        chicken_text = chicken_text.replace("ihidefiehedefe", "iehiedefie")
+        chicken_text = chicken_text.replace("ehedefeuhudefu", "euheudefeu")
+        chicken_text = chicken_text.replace("ehedefeihidefi", "eiheidefei")
+        chicken_text = String(chicken_text).charAt(0).toUpperCase() + String(chicken_text).slice(1);
+        res.send(chicken_text)
 
   } catch (err) {
     console.log(err);
